c47965929f82e402dbc08d97a6de22440736f2ce
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-18 01:58:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  array = []
+  attendees.each do |name|
+    return "Hello, my name is #{name}."
+    array << name
+  end
+  array
+end
+
+def assign_rooms(attendees)
+  room = []
+  attendees.each+with_index do |attendees, index|
+    puts "Hello, #{attendees}! You'll be assigned to room #{index}!"
+    room[attendees] = index
+  end
+end
+
+def printer
+  batch_badge_creator
+  assign_rooms
+end
